b3e590b7a7aa29863aecb8cb785c6679123d7a4d
Capericita2023 <ivonnetclaudia@gmail.com>
2024-11-30 16:21:17 -0500

    sixth-commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,26 @@
 function redirectToWhatsApp() {
-    const number = "+5358948276";
+    const number = "+5351130037";
     const message = "Hola, quisiera reservar una cita.";
     const url = `https://wa.me/${number}?text=${encodeURIComponent(message)}`;
     window.open(url, '_blank');
+}
+
+function openModal(src) {
+    const modal = document.getElementById('modal');
+    const modalImage = document.getElementById('modal-image');
+    modalImage.src = src; // Asigna la fuente de la imagen ampliada
+    modal.style.display = 'flex'; // Muestra la ventana emergente
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none'; // Oculta la ventana emergente
+}
+
+function ShowGallery(id) {
+    const divs = document.querySelectorAll('.gallery-section');
+    divs.forEach(div => {
+        div.style.display = 'none'; 
+    });
+    document.getElementById(id).style.display = 'block';
 }
